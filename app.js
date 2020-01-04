2dbc87a02f6b4d6afeaa217ad85849c56d4af9c7
Sasikumar95 <sasimymails@gmail.com>
2020-01-04 23:20:07 +0530

    nodejs installation

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-var express =require('express');
-var http =require('http');
-var fs= require('fs');
+var express = require('express');
+var http = require('http');
+var fs = require('fs');
 
-var app=express();
-var server =http.createServer(app);
+var app = express();
+var server = http.createServer(app);
 
 
-app.get('/',function(req,res){
-res.send('<h1>Hi Sasikumar</h1>');
+app.get('/', function (req, res) {
+    res.send('<h1>Hell World</h1>');
 })
 
 
-app.get('/test',function(req,res){
-    fs.readFile('./data.json',function(err, data){
-        var taskdata= JSON.parse(data.toString()).test;
-res.json(taskdata);
+app.get('/data', function (req, res) {
+    fs.readFile('./data.json', function (err, data) {
+        var taskdata = JSON.parse(data.toString()).test;
+        res.json(taskdata);
     });
-    });
-    
+});
 
-server.listen(1234,function(){
+
+server.listen(1234, function () {
     console.log("server started 1234");
 })
